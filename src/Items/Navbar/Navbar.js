817592c562bf5d6a78159c8c55d267ac00084bf7
Lua19 <miguellua100@gmail.com>
2022-04-19 16:58:31 -0600

    Added an ugly navbar

---
+++ src/Items/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './Navbar.css'
+
+export default function  Navbar() {
+    return(
+    <div className="Navbar">
+        <div className="img-container"><img src="#"></img></div>
+        <h1>Sinko</h1>
+        <h1>Sinko</h1>
+        <h1>Sinko</h1>
+        <h1>Sinko</h1>
+        <h1>Sinko</h1>
+    </div>
+    )
+}
